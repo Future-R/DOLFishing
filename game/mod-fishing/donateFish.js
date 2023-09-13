beccd4818bfbcd58030c1f3804c20b1c0b8acb64
未来菌 <1045740922@qq.com>
2023-09-14 02:31:26 +0800

    feat: 图鉴文案优化

--- game/mod-fishing/donateFish.js
+++ game/mod-fishing/donateFish.js
@@ -1,15 +1,12 @@
 
 // 此处的index是currentFish的index
 window.donateFish = function donateFish(index) {
-	console.log(index);
 	let _thisFish = V.currentFish[index];
-	console.log(_thisFish);
 	// 持有鱼[index]加入图鉴
 	V.fishCollections.push(_thisFish);
 	// 持有鱼移除index
 	V.currentFish.splice(index, 1);
 	// 结算价钱
 	let _price = V.FishModel[_thisFish].prices * 100;
-	console.log(_price);
 	return _price;
 }
