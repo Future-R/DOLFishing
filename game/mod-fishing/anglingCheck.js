f7492e943e6096e929154a5cc4163d9323294e23
未来菌 <1045740922@qq.com>
2023-09-17 12:49:27 +0800

    feat: 实装遭遇判定

--- game/mod-fishing/anglingCheck.js
+++ game/mod-fishing/anglingCheck.js
@@ -1,19 +1,26 @@
 // 钓鱼检定
 // 当前鱼篓currentFish  捉到了新鱼newFish  捕捉描述catchDesc
-function anglingCheck(fishes) {
+function anglingCheck(fishes, encounterBonus = 0) {
+
+	if (typeof (fishes) == "undefined" || fishes.length < 1) return false;
+	// 护理水平决定遭遇率加成，基础20%，S60%。
+	var _encounter = V.tending / 25 + 20;
+	// 运河、码头-20%遭遇率；钓鱼岩+20%遭遇率。
+	_encounter += encounterBonus;
+	//雨天加成20%遭遇率；猫化加成20%遭遇率；
+	if (V.weather == 'rain') _encounter += 20;
+	if (V.cat >= 6) _encounter += 20;
+
+	// 钓鱼检定
+	if (V.rng > _encounter) return false;
 	
-	if (typeof(fishes) == "undefined"||fishes.length < 1) return false;
-	//TODO: 根据护理水平决定遭遇率加成，基础40%
-	//TODO: 雨天加成20%遭遇率；猫化加成20%遭遇率
 	//TODO: 以 (1200/prices) 作为随机抽取权重
 	V.Fishing.catchDesc = "";
 	var _fishIndex = fishes[V.rng % fishes.length].index;
-	if (V.Fishing.fishCollections.includes(_fishIndex) || V.Fishing.currentFish.includes(_fishIndex))
-	{
+	if (V.Fishing.fishCollections.includes(_fishIndex) || V.Fishing.currentFish.includes(_fishIndex)) {
 		V.Fishing.newFish = false;
 	}
-	else
-	{
+	else {
 		V.Fishing.newFish = true;
 	}
 	V.Fishing.currentFish.push(_fishIndex);
